5fd61c191632ae30df9c64efba771498f0435848
Michael Pearce <mdpearce@gmail.com>
2016-03-05 22:18:21 +1100

    Initial commit

---
+++ battleassist/src/main/java/com/neaniesoft/gmbattleassist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.neaniesoft.gmbattleassist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
